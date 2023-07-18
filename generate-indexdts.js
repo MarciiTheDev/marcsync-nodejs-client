4f4a6c7f23afc8ea3de25cdc82d75921d4ff69cc
Marcel Lorbeer <admin@marcthedev.it>
2023-07-18 17:37:18 +0200

    Fixed Issue #1 & updated Version

--- generate-indexdts.js
+++ generate-indexdts.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 
 const files = []
 
-fs.readdirSync("dist").filter( f => f.endsWith(".d.ts") ).forEach( f => {
-    files.push(`export * from "./${f}"`)
+fs.readdirSync("dist").filter( f => f.endsWith(".js") ).forEach( f => {
+    files.push(`export * from "./${f.split(".")[0]}"`)
 });
 
 fs.writeFileSync("dist/index.d.ts", files.join("\n"))
